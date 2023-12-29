0df71b7a53514910f6fb0e396fdcb1d105089fe0
DDATMOUSE00 <ddatmouse00@gmail.com>
2023-12-30 03:38:06 +0900

    fail
    fail..

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField NickNameInput;
+    public GameObject Start;
+
+    void Awake()
+    {
+        Screen.SetResolution(960, 540, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
+    }
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 10 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
